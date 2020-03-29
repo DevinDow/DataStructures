9260a1d2498cbe446f78da87d09c799820a419a6
DevinDow <DevinDow@gmail.com>
2020-03-29 15:06:16 -0700

    Add project files.

---
+++ DataStructures/CompleteBinaryTree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace DataStructures
+{
+    /// <summary>
+    /// Implemented using an Array.
+    /// </summary>
+    /// <typeparam name="T">Type of Generic Data</typeparam>
+    public abstract class CompleteBinaryTree<T>
+    {
+        // Fields
+        protected ArrayList array = new ArrayList();
+
+
+        // Constructors
+        public CompleteBinaryTree() { }
+
+
+        // Public Methods
+        protected T GetData(int index)
+        {
+            return (T)array[index];
+        }
+
+        protected void SetData(int index, T data)
+        {
+            array[index] = data;
+        }
+
+
+        // Private Methods
+        protected static int CalcParentIndex(int i)
+        {
+            return (i - 1) / 2;
+        }
+        protected static int CalcLeftChildIndex(int i)
+        {
+            return i * 2 + 1;
+        }
+        protected static int CalcRightChildIndex(int i)
+        {
+            return CalcLeftChildIndex(i) + 1;
+        }
+    }
+}
---
+++ DataStructures/Heap.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+
+namespace DataStructures
+{
+    /// <summary>
+    /// Abstract Heap is the Base for MaxHeap & MinHeap, which only need to override NeedToSwap().
+    /// It is implemented as a Complete Binary Tree using an Array.
+    /// </summary>
+    /// <typeparam name="T">Type of Generic Data</typeparam>
+    public abstract class Heap<T> : CompleteBinaryTree<T>
+    {
+        // Fields
+        protected IComparer comparer;
+
+
+        // Constructors
+        public Heap(IComparer comparer)
+        {
+            this.comparer = comparer;
+        }
+
+
+        // Public Methods
+        public void Insert(T data)
+        {
+            // Add new node to end
+            int i = array.Count;
+            array.Add(data);
+            bubbleUp(i);
+        }
+
+        public T Extract()
+        {
+            // get data at root
+            T root = GetData(0);
+
+            // replace root with last element
+            int lastIndex = array.Count - 1;
+            T last = GetData(lastIndex);
+            array.RemoveAt(lastIndex);
+
+            return root;
+        }
+
+
+        // Private Methods
+        private void bubbleUp(int childIndex)
+        {
+            int parentIndex = CalcParentIndex(childIndex);
+            T parentData = GetData(parentIndex);
+            T childData = GetData(childIndex);
+            if (FirstShouldBeCloserToRoot(childData, parentData))
+            {
+                // swap & recurse
+                SetData(parentIndex, childData);
+                SetData(childIndex, parentData);
+                if (parentIndex < 0)
+                    bubbleUp(parentIndex);
+            }
+        }
+
+        private void trikleDown(int parentIndex)
+        {
+            T parentData = GetData(parentIndex);
+            int leftChildIndex = CalcLeftChildIndex(parentIndex);
+            T leftData = GetData(leftChildIndex);
+            int rightChildIndex = CalcRightChildIndex(parentIndex);
+            T rightData = GetData(rightChildIndex);
+
+            if (FirstShouldBeCloserToRoot(leftData, rightData))
+            {
+                if (FirstShouldBeCloserToRoot(leftData, parentData))
+                {
+                    // swap & recurse
+                    SetData(parentIndex, leftData);
+                    SetData(leftChildIndex, parentData);
+                    trikleDown(leftChildIndex);
+                }
+            }
+            else
+            {
+                if (FirstShouldBeCloserToRoot(rightData, parentData))
+                {
+                    // swap & recurse
+                    SetData(parentIndex, rightData);
+                    SetData(rightChildIndex, parentData);
+                    trikleDown(rightChildIndex);
+                }
+            }
+
+        }
+
+
+        // Abstract Methods
+        protected abstract bool FirstShouldBeCloserToRoot(T a, T b);
+    }
+
+    public class MaxHeap<T> : Heap<T>
+    {
+        public MaxHeap(IComparer comparer) : base(comparer) { }
+
+        protected override bool FirstShouldBeCloserToRoot(T a, T b)
+        {
+            return comparer.Compare(a, b) > 0;
+        }
+    }
+
+
+    public class MinHeap<T> : Heap<T>
+    {
+        public MinHeap(IComparer comparer) : base(comparer) { }
+
+        protected override bool FirstShouldBeCloserToRoot(T a, T b)
+        {
+            return comparer.Compare(a, b) < 0;
+        }
+    }
+}
